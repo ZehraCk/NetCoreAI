c29570f709052e8a456a3b3227c540448c914dc5
Zehra Çetinkaya <cetinkayazehra13@gmail.com>
2026-09-08 11:06:45 +0300

    İlk commit

---
+++ NetCoreAI.Project1_ApiDemo/Context/ApiContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreAI.Project1_ApiDemo.Entities;
+namespace NetCoreAI.Project1_ApiDemo.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ZEHRA\\SQLEXPRESS; initial catalog=ApiAIDb; integrated security=true; trustservercertificate=true");
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
